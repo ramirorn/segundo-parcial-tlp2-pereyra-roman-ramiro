d345a0ad3948a2a76846f2ef2b1e75bb0c50144a
Ramiro <ramiroroman306@gmail.com>
2025-11-14 16:40:27 -0300

    Se completo el fetch de los SuperHeroes

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,50 +1,67 @@
+import { useEffect, useState } from "react";
+import { Loading } from "../components/Loading";
+
 export const HomePage = () => {
   // TODO: Integrar lógica para obtener superhéroes desde la API
   // TODO: Implementar useState para almacenar la lista de superhéroes
   // TODO: Implementar función para recargar superhéroes
+  const [superHeroes, setSuperHeroes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [buttonTouched, setButtonTouched] = useState(false);
+  const [profile, setProfile] = useState(null);
+  const getProfile = async () => {
+    setLoading(true);
+    setError("");
+    try {
+      const profileResponse = await fetch("http://localhost:3000/api/profile", {
+        credentials: "include",
+      });
+      const dataProfile = await profileResponse.json();
+      console.log(dataProfile);
+      if (dataProfile) {
+        setProfile(data.name);
+      }
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-  // Datos de ejemplo para las cards
-  const superheroes = [
-    {
-      id: 1,
-      superhero: "Superman",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/644-superman.jpg",
-    },
-    {
-      id: 2,
-      superhero: "Batman",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/70-batman.jpg",
-    },
-    {
-      id: 3,
-      superhero: "Wonder Woman",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/720-wonder-woman.jpg",
-    },
-    {
-      id: 4,
-      superhero: "Spider-Man",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/620-spider-man.jpg",
-    },
-    {
-      id: 5,
-      superhero: "Iron Man",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/346-iron-man.jpg",
-    },
-    {
-      id: 6,
-      superhero: "Captain America",
-      image:
-        "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/images/lg/149-captain-america.jpg",
-    },
-  ];
+  const getheroesList = async () => {
+    setLoading(true);
+    setError("");
+    // setButtonTouched(false);
+    try {
+      const response = await fetch("http://localhost:3000/api/superheroes", {
+        credentials: "include",
+      });
+      const data = await response.json();
+
+      if (data) {
+        setSuperHeroes(data.data);
+      }
+    } catch (err) {
+      console.log(err);
+      setError("No hay superheroes para mostrar");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getheroesList();
+    getProfile();
+  }, []);
+
+  if (loading) return <Loading />;
 
   return (
     <div className="container mx-auto px-4 pb-8">
+      <h1 className="text-4xl font-bold text-center mt-8 mb-4 text-gray-800">
+        Bienvenido {profile}
+      </h1>
       <h1 className="text-4xl font-bold text-center mt-8 mb-4 text-gray-800">
         Galería de Superhéroes
       </h1>
@@ -52,6 +69,7 @@ export const HomePage = () => {
       <div className="flex justify-center mb-8">
         <button
           onClick={() => {
+            getheroesList();
             // TODO: Implementar función para recargar superhéroes
           }}
           className="bg-blue-600 hover:bg-blue-700 text-white font-semibold px-6 py-2 rounded transition-colors"
@@ -61,7 +79,7 @@ export const HomePage = () => {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {superheroes.map((hero) => (
+        {superHeroes.map((hero) => (
           <div
             key={hero.id}
             className="bg-white rounded-lg shadow-md overflow-hidden hover:shadow-xl hover:scale-105 transition-all duration-300 cursor-pointer"
